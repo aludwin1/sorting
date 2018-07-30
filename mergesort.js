c451619ee99f483614c6c014f477248439f75aae
Aaron Ludwin <aludwin1@binghamton.edu>
2018-07-30 18:29:07 -0400

    cleaned up and made more concise

--- mergesort.js
+++ mergesort.js
@@ -2,18 +2,17 @@ const merge = (left, right, arr) => {
   let index = 0;
   while(index < arr.length) {
     let smallest = left[0] > right[0] ? right : left;
-    if(left.length && right.length) arr[index] = smallest.shift();
-    else if (left.length) arr[index] = left.shift();
-    else arr[index] = right.shift();
-    index++;
+    if(left.length && right.length) arr[index++] = smallest.shift();
+    else if (left.length) arr[index++] = left.shift();
+    else arr[index++] = right.shift();
   }
   return arr;
 };
 
 const mergeSort = arr => {
   if(arr.length <= 1) return arr;
-  let midpoint = Math.ceil(arr.length / 2);;
-  let left = mergeSort(arr.slice(0, midpoint));
-  let right = mergeSort(arr.slice(midpoint));
+  const midpoint = Math.ceil(arr.length / 2);
+  const left = mergeSort(arr.slice(0, midpoint));
+  const right = mergeSort(arr.slice(midpoint));
   return merge(left, right, arr);
 };
