2d654abc0107a3630604ff90f08df37a03908741
Aaron Ludwin <aludwin1@binghamton.edu>
2018-07-30 14:37:31 -0400

    corrected to O(N)

--- bubblesort.js
+++ bubblesort.js
@@ -1,8 +1,12 @@
 const bubbleSort = arr => {
-  for(let i = 0; i < arr.length; i++) {
-    for(let j = 0; j < arr.length; j++) {
-      if(arr[i] < arr[j]) [arr[j], arr[i]] = [arr[i], arr[j]];
+  let end = arr.length;
+  while(end > 1) {
+    for(let i = 0; i < (end - 1); i++) {
+      if(arr[i] > arr[i + 1]) {
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+      }
     }
+    end--;
   }
   return arr;
 };
